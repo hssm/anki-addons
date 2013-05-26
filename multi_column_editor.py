2de2d40a378d012fc1bda24540a40aeb7e1f81e7
Houssam Salem <houssam.salem.au@gmail.com>
2013-05-26 22:38:24 +1000

    Make column count configurable from the UI.

--- multi_column_editor.py
+++ multi_column_editor.py
@@ -2,24 +2,32 @@
 # Version: 1.0
 # See github page to report issues or to contribute:
 # https://github.com/hssm/anki-addons
- 
+
+from aqt import *
+
+from anki.hooks import wrap
 import aqt.editor
+from aqt.editor import Editor
 
 # The approach here is to still let the old setFields function run as usual,
 # but copy the HTML it generates into our own table. We do this instead of
-# overriding the entire function for future-proofing; the content of the 
+# overriding the entire function for future-proofing; the content of the
 # tables could easily change.
 
-# TODO: make column count configurable.
+CONF_KEY_COLUMN_COUNT = 'multi_column_count'
 
 aqt.editor._html = aqt.editor._html + """
 <script>
+var columnCount = 1;
+
+function setColumnCount(n) {
+    columnCount = n;
+}
+
 var origSetFields = setFields;
-var mySetFields = function(fields, focusTo) {
+var mySetFields = function setFields(fields, focusTo) {
     origSetFields(fields, focusTo);
-    
-    var columnCount = 2;
-    
+        
     // In the original, there is a row for each field's name followed by a row
     // with that field's edit box. What we do is make an array of each name
     // and an array of each field, then lay them out again with n number of
@@ -35,8 +43,8 @@ var mySetFields = function(fields, focusTo) {
     
     // And these are the edit boxes themselves.
     $("#fields tr:nth-child(even) td").each(function () {
-        // BUT! They have width=100. Make it auto so they look nice.
-        $(this).attr('width', 'auto');
+        // Make them evenly-spaced
+        $(this).attr('width', 100/columnCount+'%%');
         fEdit.push(this.outerHTML);
     });
     
@@ -71,3 +79,32 @@ var mySetFields = function(fields, focusTo) {
 var setFields = mySetFields;
 </script>
 """
+
+def onColumnCountChanged(editor, count):
+    mw.pm.profile[CONF_KEY_COLUMN_COUNT] = count
+    editor.loadNote()
+
+def myEditorInit(self, mw, widget, parentWindow, addMode=False):
+
+    count = mw.pm.profile.get(CONF_KEY_COLUMN_COUNT, 1)
+
+    # TODO: These don't deserve their own row. Maybe place it next to the
+    # tag editor.
+    l = QLabel("Columns")
+    n = QSpinBox(self.widget)
+    n.setValue(count)
+    n.connect(n,
+              SIGNAL("valueChanged(int)"),
+              lambda value: onColumnCountChanged(self, value))
+    n.setMaximum(20)
+    n.setMinimum(1)
+    self.outerLayout.addWidget(l)
+    self.outerLayout.addWidget(n)
+
+
+def myLoadNote(self):
+    count = mw.pm.profile.get(CONF_KEY_COLUMN_COUNT, 1)
+    self.web.eval("setColumnCount(%d);" % count)
+
+Editor.__init__ = wrap(Editor.__init__, myEditorInit)
+Editor.loadNote = wrap(Editor.loadNote, myLoadNote, pos="before")
