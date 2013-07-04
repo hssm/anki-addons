606b59f6c2dc83a3709a4b28299520aacee73889
Houssam Salem <houssam.salem.au@gmail.com>
2013-07-04 19:35:46 +1000

    Use fixed-width table. - Manually account for FrozenFields if it's there. - Load values using bridge since we can't rely on wrapping onLoad since other add-ons (e.g., FrozenFields) override it.

--- multi_column_editor.py
+++ multi_column_editor.py
@@ -9,36 +9,29 @@ from aqt.editor import Editor
 import aqt.editor
 
 
+# A Sensible maximum number of columns we are able to set
+MAX_COLUMNS = 18
+
+# Settings key to remember column count
 CONF_KEY_COLUMN_COUNT = 'multi_column_count'
+
+# Flag to enable hack to make Frozen Fields look normal
+ffFix = False
 
 aqt.editor._html = aqt.editor._html + """
 <script>
-s  = '<style>'
-s += '.mceCell > * { display: table-cell; vertical-align: middle; }'
-s += '</style>'
-
-$('html > head').append(s);
-
-(function($) {
-    $.fn.changeElementType = function(newType) {
-        var attrs = {};
-    
-        $.each(this[0].attributes, function(idx, attr) {
-            attrs[attr.nodeName] = attr.nodeValue;
-        });
-    
-        this.replaceWith(function() {
-            return $("<" + newType + "/>", attrs).append($(this).contents());
-        });
-    };
-})(jQuery);
-
-
+var columnCount = 1;
 function setColumnCount(n) {
     columnCount = n;
 }
 
-var makeColumns = function(event) {
+var ffFix = false; // Frozen Fields fix
+function setFFFix(use) {
+  ffFix = use;
+}
+
+// Event triggered when #fields is modified.
+function makeColumns(event) {
 
     // If the inserted object is not at the top level of the "fields" object,
     // ignore it. We're assuming that anything added to the "fields" table is
@@ -51,34 +44,36 @@ var makeColumns = function(event) {
     // with that field's edit box. I.e.:
     // <tr><td>...Field name...</td></tr>
     // <tr><td>...Edit box...</td></tr>
-    // What we do is copy the content (inside <tr>) of the field name rows into
-    // an array, then copy the content of the edit box rows into a separate
-    // array, then lay them out again with n number of elements in each row,
-    // where n = columnCount.
+    // We copy the content of each row into its own group's array and then
+    // write out the table again using our own ordering.
+
+    // Inject global variables for us to use from python.
+    py.run("mceTrigger");
     
     var fNames = [];
     var fEdit = [];
 
-    $("#fields tr:nth-child(odd) > td").each(function (){
-        $(this).changeElementType("span");
-    });
-    
-    $("#fields tr:nth-child(even) > td").each(function (){
-        $(this).changeElementType("span");
-    });
-    
+    // Hack to make Frozen Fields look right.
+    if (ffFix) {
+        // Apply fixed width to first <td>, which is a Frozen Fields cell,
+        // or it will take up too much space.
+        $("#fields td:first-child").each(function (){
+            $(this).attr("width", "28px");
+        });
+    }
+
     $("#fields tr:nth-child(odd)").each(function (){
-        fNames.push("<td class='mceCell'>" + this.innerHTML + "</td>");
+        fNames.push(this.innerHTML);
     });
     
     $("#fields tr:nth-child(even)").each(function (){
-        fEdit.push("<td class='mceCell'>" + this.innerHTML + "</td>");
+        fEdit.push(this.innerHTML);
     });
     
     txt = "";
     for (var i = 0; i < fNames.length;) {
         // A row of names
-        txt += "<tr>";
+        txt += "<tr class='mceRow'>";
         for (var j = 0; j < columnCount; j++) {
             var td = fNames[i + j];
             if (td === undefined) {
@@ -89,7 +84,7 @@ var makeColumns = function(event) {
         txt += "</tr>";
         
         // A row of edit boxes
-        txt += "<tr>";
+        txt += "<tr class='mceRow'>";
         for (var j = 0; j < columnCount; j++) {
             var td = fEdit[i + j];
             if (td === undefined) {
@@ -98,55 +93,63 @@ var makeColumns = function(event) {
             txt += td;
         }
         txt += "</tr>";
-        
         i += columnCount;
     }
-
+    
     // Unbind then rebind to avoid infinite loop
     $('#fields').unbind('DOMNodeInserted')
-    $("#fields").html("<table cellpadding=0 width=100%%>" + txt + "</table>");
+    $("#fields").html(
+        "<table style='table-layout: fixed;' width=100%%>" + txt + "</table>"
+    );
     $('#fields').bind('DOMNodeInserted', makeColumns);
-        
-    $(".mceCell").css({'width' : (100/columnCount)+'%%'});
-};
-// Restructure the table after it is populated
-$('#fields').bind('DOMNodeInserted', makeColumns);
+}
 
+// Attach event to restructure the table after it is populated
+$('#fields').bind('DOMNodeInserted', makeColumns);
 </script>
 """
 
 def onColumnCountChanged(editor, count):
+    "Save column count to settings and re-draw with new count."
     mw.pm.profile[CONF_KEY_COLUMN_COUNT] = count
-    editor.web.eval("setColumnCount(%d);" % count)
     editor.loadNote()
 
 def myEditorInit(self, mw, widget, parentWindow, addMode=False):
     count = mw.pm.profile.get(CONF_KEY_COLUMN_COUNT, 1)
 
     # TODO: These don't deserve their own row. Maybe place it next to the
-    # tag editor.
+    # tag editor (if I ever figure out how).
     l = QLabel("Columns")
     n = QSpinBox(self.widget)
     n.setValue(count)
     n.connect(n,
               SIGNAL("valueChanged(int)"),
               lambda value: onColumnCountChanged(self, value))
-    n.setMaximum(15)
+    n.setMaximum(MAX_COLUMNS)
     n.setMinimum(1)
     self.outerLayout.addWidget(l)
     self.outerLayout.addWidget(n)
+    
+    # If the user has the Frozen Fields add-on installed, tweak the
+    # layout a bit to make it look right.
+    global ffFix
+    try:
+        __import__("Frozen Fields")
+        ffFix = True
+    except:
+        pass
 
 
-# Set the initial column count on load
-loaded = False
-def onProfileLoad():
-    global loaded
-    if not loaded:
+def myBridge(self, str):
+    """
+    Called from JavaScript to inject some values before it needs
+    them.
+    """
+    if str == "mceTrigger":
         count = mw.pm.profile.get(CONF_KEY_COLUMN_COUNT, 1)
-        print "Initial column count: ", count
-        s = "<script>var columnCount =" + str(count) + ";</script>"
-        aqt.editor._html += s
-        loaded = True
-
+        self.web.eval("setColumnCount(%d);" % count)
+        if ffFix:
+            self.web.eval("setFFFix(true)")
+        
 Editor.__init__ = wrap(Editor.__init__, myEditorInit)
-addHook("profileLoaded", onProfileLoad)
+Editor.bridge = wrap(Editor.bridge, myBridge, 'before')
